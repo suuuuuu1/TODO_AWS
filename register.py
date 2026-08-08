d173f6a2cdcdd5ba7a1ade5c62dd3f20ccaad4aa
LpiYs <qazplmsantacruz@gmail.com>
2026-08-08 15:29:22 +0900

    todoのコマンドを追加

--- register.py
+++ register.py
@@ -8,6 +8,8 @@ commands = [
     {"name": "view", "type": 1, "description": "自分のTODO一覧を見る"},
     {"name": "add", "type": 1, "description": "タスクを追加する（フォームが開くよ）"},
     {"name": "rm", "type": 1, "description": "タスクを消す（選んで削除）"},
+    {"name": "done", "type": 1, "description": "タスクに完了設定を付ける"}
+
 ]
 
 r = requests.put(
